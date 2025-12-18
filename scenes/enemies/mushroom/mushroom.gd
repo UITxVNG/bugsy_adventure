6858c9d7cb46821ed61fea29a2495e5f099ec8be
ThnhHuynh04 <22521406@gm.uit.edu.vn>
2025-12-18 11:27:14 +0700

    feat: add push area to mushroom enemy to push player

--- scenes/enemies/mushroom/mushroom.gd
+++ scenes/enemies/mushroom/mushroom.gd
@@ -1,5 +1,13 @@
 extends EnemyCharacter
 
+@export var push_force: float = 300.0
+
 func _ready() -> void:
 	fsm = FSM.new(self, $States, $States/Run)
 	super._ready()
+
+func _on_push_area_body_entered(body: Node2D) -> void:
+	print(body)
+	if body is Player:
+		var dir = (body.global_position - global_position).normalized()
+		body.velocity.x = dir.x * push_force
